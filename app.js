f31221dded6a4d25d42fd29128be3e676a9a62eb
Jeroen Wienk <jeroenwienk@gmail.com>
2021-03-27 20:55:10 +0100

    feat: add discord bot

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 'use strict';
 
 const Homey = require('homey');
+const { HomeyAPI } = require('athom-api');
+
+const { DiscordBot } = require('./lib/discord/DiscordBot');
 
 if (process.env.DEBUG === '1') {
-  require('inspector').open(9229, '0.0.0.0', false);
+  require('inspector').open(9230, '0.0.0.0', false);
 }
 
-class MyApp extends Homey.App {
+class UtilsApp extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('UtilsApp has been initialized');
+
+    this.homeyAPI = await HomeyAPI.forCurrentHomey(this.homey);
+
+    this.discordBot = new DiscordBot({
+      homey: this.homey,
+      homeyAPI: this.homeyAPI,
+    });
+
+    this.discordBot.init();
   }
 }
 
-module.exports = MyApp;
+module.exports = UtilsApp;
